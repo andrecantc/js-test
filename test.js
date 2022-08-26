d6b778ca084dd730b70586fdf8216836a759277c
André Gudet <andre.gaudet.ag@gmail.com>
2022-08-26 15:36:44 +0200

    js-test

---
+++ test.js
@@ -0,0 +1,11 @@
+//console.log("hej")
+//console.log("då")
+//console.log("true")
+//console.log("false")
+//var ps = require("prompt-sync");
+//var prompt = ps();
+
+//let name = prompt("Enter your name: ");
+var ps = require("prompt-sync");
+var prompt = ps();
+
