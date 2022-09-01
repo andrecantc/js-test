424b6e1391d7905da142f37e367fa9e953083bd2
André Gudet <andre.gaudet.ag@gmail.com>
2022-09-01 09:01:38 +0200

    cs update

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-//console.log("hej")
-//console.log("då")
-//console.log("true")
-//console.log("false")
-//var ps = require("prompt-sync");
-//var prompt = ps();
+// console.log("hej")
+// console.log("då")
+// console.log("true")
+// console.log("false")
+// var ps = require("prompt-sync");
+// var prompt = ps();
 
-//let name = prompt("Enter your name: ");
+
 var ps = require("prompt-sync");
 var prompt = ps();
+let input = prompt("Ludde är sämtst på cs: "); 
+
+if (input=="ja") {
+    console.log ("rätt")
+} else {
+    console.log ("rätt")
+}
+if (input=="nej") {
+    console.log("fel svar")
+}
 
