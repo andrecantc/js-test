c3e917a3b716c95073c1620c226aaa95cf8e903f
André Gudet <andre.gaudet.ag@gmail.com>
2022-09-01 09:30:27 +0200

    ludde kan inte spela

--- test.js
+++ test.js
@@ -8,12 +8,11 @@
 
 var ps = require("prompt-sync");
 var prompt = ps();
-let input = prompt("Ludde är sämtst på cs: "); 
+let input = prompt("Ludde är sämst på cs: "); 
 
 if (input=="ja") {
     console.log ("rätt")
-} else {
-    console.log ("rätt")
+
 }
 if (input=="nej") {
     console.log("fel svar")
